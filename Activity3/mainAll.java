f1674d2613ef90ed21b44b2e144a581d9dee92f3
Jay Wesley M. Decipeda <45021145+Itsjustwesleyd@users.noreply.github.com>
2019-01-28 13:53:08 +0800

    Update mainAll.java

--- Activity3/mainAll.java
+++ Activity3/mainAll.java
@@ -8,7 +8,21 @@ public class mainAll{
          student.setIdNumber("18-2977-319");
          student.setFirstname("Christopher");
          student.setLastName("Bermudez");
+         student.printInformation();
+      
+         StudentInfo student = new StudentInfo("CITCS", "BSCS", "Something", 2, "2018 - 2019");
+         student.setIdNumber("18-1921-981");
+         student.setFirstname("Jay Wesley");
+         student.setLastName("Decipeda");
          student.printInformation(); 
+      
+         StudentInfo student = new StudentInfo("CITCS", "BSCS", "Something", 2, "2018 - 2019");
+         student.setIdNumber("18-7195-425");
+         student.setFirstname("Castano Tome");
+         student.setLastName("Teofilo");
+         student.printInformation(); 
+      
+      
     }
     
     
